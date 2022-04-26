ca3d00d34aae62b1c9363d8a9ed92d7e31ba2cb4
Niki Ewald Zakariassen <nikiewald@hotmail.dk>
2022-04-27 01:17:33 +0200

    Added term stub

---
+++ stubs/peripherals/term.lua
@@ -0,0 +1,20 @@
+--- @class term
+term = {}
+
+function term.write(text) end
+function term.blit(text, textColors, backgroundColors) end
+function term.clear() end
+function term.clearLine() end
+function term.getCursorPos() end
+function term.setCursorPos(x, y) end
+function term.setCursorBlink(bool) end
+function term.isColor() end
+function term.getSize() end
+function term.scroll(n) end
+function term.redirect(target) end
+function term.current() end
+function term.native() end
+function term.setTextColor(color) end
+function term.getTextColor() end
+function term.setBackgroundColor(color) end
+function term.getBackgroundColor() end
