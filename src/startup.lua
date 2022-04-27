754a66c7537c79980ef49259e2fb54795ffb9da2
Niki Ewald Zakariassen <nikiewald@hotmail.dk>
2022-04-27 17:02:11 +0200

    Updated startup script

--- src/startup.lua
+++ src/startup.lua
@@ -1,3 +1,19 @@
+--require("mocks/init");
+
 EnhancedTerminal = require("api/EnhancedTerminal");
 
-EnhancedTerminal.new("right").isColor()
+et = EnhancedTerminal.new("right");
+
+et.printList({
+    {"Dirt", 25},
+    {"Glass", 100},
+    {"Oak Planks", 1234},
+})
+
+et.setCursorPos(1, 10)
+et.write("55555")
+
+et.setCursorPos(3, 10)
+et.write("22")
+
+--et.dump()
