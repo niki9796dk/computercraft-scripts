92e99bc15a5cd0a538f178396d580b4fec7a9b19
Niki Ewald Zakariassen <nikiewald@hotmail.dk>
2022-04-27 22:36:00 +0200

    Added first attempt at a simple application loop

---
+++ src/api/ApplicationLoop.lua
@@ -0,0 +1,39 @@
+ApplicationLoop = {
+    terminate = false,
+    listeners = {
+        timers = {}
+    },
+}
+
+function ApplicationLoop:run(main)
+    main()
+
+    -- Application loop
+    while not terminate do
+        -- Keep pulling events
+        local event = {os.pullEvent()}
+
+        if event[1] == "timer" then
+            local timerFunction = ApplicationLoop:findTimerFunction(event[2])
+
+            timerFunction()
+        end
+    end
+end
+
+function ApplicationLoop:findTimerListener(timer)
+    for key, listener in pairs(ApplicationLoop.listeners.timers) do
+        if listener.timer == timer then
+            return key, listener
+        end
+    end
+
+    assert(false, "Could not locate timer listener")
+end
+
+function ApplicationLoop:timeout(time, func)
+    table.insert(ApplicationLoop.listeners.timers, {
+        timer = os.startTimer(time),
+        func = func,
+    })
+end
