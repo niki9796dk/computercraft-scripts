2b0f2b1561b46255f7b428a817b85d0daf14b6f8
Niki Ewald Zakariassen <nikiewald@hotmail.dk>
2022-04-27 22:01:54 +0200

    Added coroutine stub

---
+++ stubs/Apis/coroutine.lua
@@ -0,0 +1,9 @@
+coroutine = {}
+
+function coroutine.create(f) end
+function coroutine.resume(coro, ...) end
+function coroutine.running() end
+function coroutine.status(coro) end
+function coroutine.wrap(f) end
+function coroutine.yield(...) end
+
