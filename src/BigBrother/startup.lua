2f97fd47b448acf68c042b11ebc5033a47dcd27d
Niki Ewald Zakariassen <nikiewald@hotmail.dk>
2022-05-01 15:23:34 +0200

    Updated big brother script

--- src/BigBrother/startup.lua
+++ src/BigBrother/startup.lua
@@ -1,22 +1,50 @@
 --- @type PlayerDetector
 pd = peripheral.find("playerDetector")
 
-lastPlayerStates = {}
+playerStateManager = {
+    previousStates = {},
+    currentStates = {},
+}
 
 --- @return PlayerPos
-function getLastPlayerState(playerName, currentPlayerState)
-    return lastPlayerStates[playerName] or currentPlayerState
+function playerStateManager.getCurrentState(playerName)
+    return playerStateManager.currentStates[playerName] or error("No such player: " .. playerName)
+end
+
+--- @return PlayerPos
+function playerStateManager.getPreviousState(playerName)
+    return playerStateManager.previousStates[playerName] or error("No such player: " .. playerName)
+end
+
+function playerStateManager.hasChangedDimension(playerName)
+    return playerStateManager.getCurrentState(playerName).dimension ~= playerStateManager.getPreviousState(playerName).dimension
+end
+
+function playerStateManager.updateStateMap()
+    for _, playerName in pairs(pd.getOnlinePlayers()) do
+        liveState = pd.getPlayerPos(playerName)
+
+        playerStateManager.previousStates[playerName] = playerStateManager.currentStates[playerName] or liveState
+        playerStateManager.currentStates = liveState
+    end
+end
+
+function playerStateManager.forEachPlayer(func)
+    for _, playerName in pairs(pd.getOnlinePlayers()) do
+        func(playerName, playerStateManager.currentStates[playerName], playerStateManager.previousStates[playerName])
+    end
 end
 
 while true do
-    for _, playerName in pairs(pd.getOnlinePlayers()) do
-        currentPlayerState = pd.getPlayerPos(playerName)
-        lastPlayerState = getLastPlayerState(playerName, currentPlayerState)
+    playerStateManager.updateStateMaps()
 
-        if currentPlayerState.dimension ~= lastPlayerStates.dimension then
-            print("[BigBrother] " .. playerName .. " welcome to [%s]!")
+    ---@param currentState PlayerPos
+    ---@param previousState PlayerPos
+    playerStateManager.forEachPlayer(function (playerName, currentState, previousState)
+        if playerStateManager.hasChangedDimension(playerName) then
+            print("[BigBrother] " .. playerName .. " welcome to [" .. currentState.dimension .. "]!")
         end
-    end
+    end)
 
     sleep(1)
 end
