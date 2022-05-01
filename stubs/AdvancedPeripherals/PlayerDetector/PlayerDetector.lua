f3681d6ddfddc4d4fe854a5aaf1219e2f6aee113
Niki Ewald Zakariassen <nikiewald@hotmail.dk>
2022-05-01 15:07:20 +0200

    Added some advanced peripheals stubs

---
+++ stubs/AdvancedPeripherals/PlayerDetector/PlayerDetector.lua
@@ -0,0 +1,14 @@
+--- @class PlayerDetector
+PlayerDetector = {}
+
+--- @return PlayerPos
+function PlayerDetector.getPlayerPos(playerName) end
+
+--- @return table
+function PlayerDetector.getOnlinePlayers() end
+
+--- @return table
+function PlayerDetector.getPlayersInRange() end
+
+--- @return table
+function PlayerDetector.getPlayersInCubic() end
---
+++ stubs/AdvancedPeripherals/PlayerDetector/PlayerPos.lua
@@ -0,0 +1,10 @@
+--- @class PlayerPos
+local PlayerPos = {
+    dimension = '',
+    eyeHeight = 0,
+    pitch = 0,
+    x = 0,
+    y = 0,
+    yaw = 0,
+    z = 0,
+}
